aaa521811054867fcb2a7f63fb23c3914c84dfe7
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-04-16 17:12:33 -0400

    Add configuration for FleetCarma board (PIC32MX795F512H with default CAN pins).

--- bootloaders/BoardConfig.h
+++ bootloaders/BoardConfig.h
@@ -737,6 +737,83 @@
     #define LoadFlashWaitStates()       (CHECON = 2)                // 0 for 0-30Mhz, 1 for 31-60Mhz, 2 for 61-80Mhz
 
 //************************************************************************
+#elif defined(_BOARD_FLEETCARMA_)
+
+#if defined(PUT_CONFIG_BITS_HERE)
+
+    //*    Oscillator Settings
+    #pragma config FNOSC        = PRIPLL                            // Oscillator selection
+    #pragma config POSCMOD      = XT                                // Primary oscillator mode
+    #pragma config FPLLIDIV     = DIV_2                             // PLL input divider
+    #pragma config FPLLMUL      = MUL_20                            // PLL multiplier
+    #pragma config FPLLODIV     = DIV_1                             // PLL output divider
+    #pragma config FPBDIV       = DIV_1                             // Peripheral bus clock divider
+    #pragma config FSOSCEN      = OFF                               // Secondary oscillator enable
+
+    //*    Clock control settings
+    #pragma config IESO         = OFF                               // Internal/external clock switchover
+    #pragma config FCKSM        = CSDCMD                            // Clock switching (CSx)/Clock monitor (CMx)
+    #pragma config OSCIOFNC     = OFF                               // Clock output on OSCO pin enable
+
+    //*    Other Peripheral Device settings
+    #pragma config FWDTEN       = OFF                               // Watchdog timer enable
+    #pragma config WDTPS        = PS1024                            // Watchdog timer postscaler
+
+    //*    Code Protection settings
+    #pragma config CP           = OFF                               // Code protection
+    #pragma config BWP          = OFF                               // Boot flash write protect
+    #pragma config PWP          = OFF                               // Program flash write protect
+
+    //*    Debug settings
+    #pragma config ICESEL       = ICS_PGx2                          // ICE pin selection
+
+    //*    Other Peripheral Device settings
+    #pragma config FSRSSEL      = PRIORITY_7                        // SRS interrupt priority
+    #pragma config FCANIO       = ON                               // Standard/alternate CAN pin select (OFF=Alt)
+    #pragma config FETHIO       = ON                                // Standard/alternate ETH pin select (OFF=Alt)
+    #pragma config FMIIEN       = OFF                               // MII/RMII select (OFF=RMII)
+
+    //*    USB Settings
+    #pragma config UPLLEN       = ON                                // USB PLL enable
+    #pragma config UPLLIDIV     = DIV_2                             // USB PLL input divider
+    #pragma config FVBUSONIO    = OFF                               // VBUS pin control
+    #pragma config FUSBIDIO     = OFF                               // USBID pin control
+#endif
+
+    #define CAPABILITIES    (blCapDownloadLED | blCapUARTInterface | blCapAutoResetListening | blCapVirtualProgramButton | CAPCOMMON)
+
+    // Boot LED
+    #define EnableBootLED()             (TRISCCLR = (1 << 13))
+    #define DisableBootLED()            (TRISCSET = (1 << 13))
+    #define BootLED_Toggle()            (LATCINV = (1 << 13))
+    #define BootLED_On()                (LATCSET = (1 << 13))
+    #define BootLED_Off()               (LATCCLR = (1 << 13))
+
+    // Download LED
+    #define EnableDownLoadLED()         (TRISCCLR = (1 << 14))
+    #define DisableDownLoadLED()        (TRISCSET = (1 << 14))
+    #define DownloadLED_Toggle()        (LATCINV = (1 << 14))
+    #define DownloadLED_On()            (LATCSET = (1 << 14))
+    #define DownloadLED_Off()           (LATCCLR = (1 << 14))
+
+    // Other capabilities
+    #define LISTEN_BEFORE_LOAD          2000                // no less than 2 seconds
+    #define _USE_UART1_FOR_BOOTLOADER_                      // avrdude program UART
+    #define BAUDRATE                    115200              // avrdude baudrate
+    #define fLoadFromAVRDudeViaVirtualProgramButton (LATCbits.LATC12 == 1)
+    #define ClearVirtualProgramButton()             (LATCCLR = (1 << 12))
+
+    #define _CPU_NAME_                  "32MX795F512H"
+    #define VEND                        vendDigilent
+    #define PROD                        prodChipKITMax32
+    #define F_CPU                       80000000UL
+    #define F_PBUS                      F_CPU
+
+    #define FLASH_BYTES                 0x80000                     // 512K
+    #define FLASH_PAGE_SIZE             4096
+    #define LoadFlashWaitStates()       (CHECON = 2)                // 0 for 0-30Mhz, 1 for 31-60Mhz, 2 for 61-80Mhz
+
+//************************************************************************
 #elif defined(_BOARD_CEREBOT_32MX7_)
 
 #if defined(PUT_CONFIG_BITS_HERE)
